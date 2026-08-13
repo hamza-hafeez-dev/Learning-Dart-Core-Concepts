f1c5a47853022f2f647195d122428997f055bade
Muhammad Hamza Hafeez <official.hamzahafeez@gmail.com>
2026-08-13 23:40:45 +0500

    Implement record examples in Records.dart
    Added examples of using records in Dart, including unnamed, named, and mixed records. Also demonstrated how to use a class with records.

--- Records.dart
+++ Records.dart
@@ -1,1 +1,43 @@
+void main(List<String> arguments) {
+  // Record Ak Container Hota han Jahan hum Multiple Value ko Ak sath Store Kiya Jata Ha Bina Class banya !
 
+  (String, int, int) record;
+  record = ('Hamza', 42, 344);
+  print(record);
+  print(record.$1);
+  print(record.$3);
+
+  // Named Record :
+
+  ({String name, int age, int id}) namedRecord;
+  namedRecord = (name: 'Hamza', age: 19, id: 2233);
+  print(namedRecord);
+  print(namedRecord.name);
+  print(namedRecord.age);
+
+  // Mix Record :
+
+  var mixRecord = ('Hamza', a: 123, b: 786, 'Hafeez');
+  print(mixRecord);
+  print(mixRecord.a);
+  print(mixRecord.$1);
+  print(mixRecord.b);
+
+  // Class ka Object Call Kar ka Object ko foo Ka Under lana or Data Print Karna
+  StudentID studentID = StudentID();
+  var result = foo(studentID);
+  print(' ');
+  print(result.$1);
+  print(result.$2);
+}
+
+// Class Bana Kar Es ka Varaibles ko Record ma Add Karna :
+
+class StudentID {
+  String name = 'Hussu';
+  int rollNumber = 12;
+}
+
+(String, int) foo(StudentID s) {
+  return (s.name, s.rollNumber);
+}
